d50d8055c63c3ec32dc69be3a96e2b9d7fa8928d
urielcruzrosas <uricruzrosas@gmail.com>
2026-09-12 13:57:45 -0600

    Practice #2 and 3: Conditionals and loops

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+        Console.Write("What is your subject score? ");
+        string userScoreInput = Console.ReadLine();
+        int grade = int.Parse(userScoreInput);
+
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Adding + or -
+        string sign = "";
+        int last_digit = grade % 10;
+
+        if (last_digit >= 7)
+        {
+            sign = "+";
+        }
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // Handle the A+ grades
+        if (grade >= 93)
+        {
+            sign = "";
+        }
+
+        // Handle the F+ and F- grades
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Stay focused and you'll get it next time!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        int guessCount = 1;
+
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+        }
+
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"It took you {guessCount} guesses.");
     }
 }
+
+
+/*
+Extra Challenges
+1. Keep track of how many guesses the user has made and inform them of it at the end of the game.
+2. After the game is over, ask the user if they want to play again. Then, loop back and play the whole game again and continue this loop as long as they keep saying "yes".
+*/
