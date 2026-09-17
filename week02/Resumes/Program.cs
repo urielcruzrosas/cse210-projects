e53fcfbb47456ca74a99c0f192a4a3ff3ad9ba2e
urielcruzrosas <uricruzrosas@gmail.com>
2026-09-17 17:25:05 -0600

    Week 02: Resume practice

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "AI Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2017;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Team Lead";
+        job2._company = "Adobe";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
